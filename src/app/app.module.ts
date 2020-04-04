5c828cdcd3d15a273627dc73964fa165b90987cc
xamul-it <luigi.zanderighi@gmail.com>
2020-04-04 20:49:39 +0200

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SiteListComponent } from './site-list/site-list.component';
-
+import { SiteListComponent } from './site-list/site-list.compondent';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, SiteListComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: SiteListComponent },
+    ])
+  ],
+
+  declarations: [ 
+    AppComponent, 
+    SiteListComponent, 
+    TopComponent 
+  ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
